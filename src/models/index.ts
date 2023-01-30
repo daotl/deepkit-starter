823f74a37991c86222d16c2333a76828b1638662
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-01-30 10:31:39 +0800

    feat: update models, add models with and without relations

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,26 @@
+import type { ConditionalExcept } from 'type-fest'
+
+import {
+  Category as CategoryR,
+  Post as PostR,
+  Profile as ProfileR,
+  Role as RoleR,
+  User as UserR,
+} from './generated/interfaces'
+
 export * from './generated/interfaces'
+
+export type ModelR = CategoryR | PostR | ProfileR | RoleR | UserR
+
+export type OmitModelRelation<MR extends ModelR> = ConditionalExcept<
+  MR,
+  ModelR | ModelR[]
+>
+
+export type Category = OmitModelRelation<CategoryR>
+export type Post = OmitModelRelation<PostR>
+export type Profile = OmitModelRelation<ProfileR>
+export type Role = OmitModelRelation<RoleR>
+export type User = OmitModelRelation<UserR>
+
+export { CategoryR, PostR, ProfileR, RoleR, UserR }
